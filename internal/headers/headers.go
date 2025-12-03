2aed7e18074db08c54bddba2ca69d27411712c45
rowsedgy <rowsedgy@gmail.com>
2025-12-03 18:02:13 +0100

    byte bullshit badly explained

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -2,31 +2,66 @@ package headers
 
 import (
 	"bytes"
+	"fmt"
 	"strings"
 )
 
+const crlf = "\r\n"
+
 type Headers map[string]string
 
+func NewHeaders() Headers {
+	return map[string]string{}
+}
+
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
-	if h == nil {
-		h = make(map[string]string)
+	idx := bytes.Index(data, []byte(crlf))
+	if idx == -1 {
+		return 0, false, nil
+	}
+	if idx == 0 {
+		return 2, true, nil
 	}
 
-	totalConsumed := 0
-
-	for {
-		idx := bytes.Index(data[totalConsumed:], []byte("\r\n"))
-		if idx == -1 {
-			return totalConsumed, false, nil
-		}
-
-		line := string(data[totalConsumed : totalConsumed+idx])
-		totalConsumed += idx + 2
-
-		if line == "" {
-			return totalConsumed, true, nil
-		}
-
-		parts := strings.SplitN()
+	parts := bytes.SplitN(data[:idx], []byte(":"), 2)
+	key := string(parts[0])
+	if key != strings.TrimRight(key, " ") {
+		return 0, false, fmt.Errorf("invalid header name: %s", key)
 	}
+
+	value := bytes.TrimSpace(parts[1])
+	key = strings.TrimSpace(key)
+
+	if checkValidToken(key) {
+		h.Set(key, string(value))
+	} else {
+		return 0, false, fmt.Errorf("invalid header format: %s", key)
+	}
+
+	return idx + 2, false, nil
+}
+
+func (h Headers) Set(key, value string) {
+	key = strings.ToLower(key)
+	v, ok := h[key]
+	if ok {
+		value = fmt.Sprintf("%s, %s", v, value)
+	}
+	h[key] = value
+}
+
+func checkValidToken(s string) bool {
+	const allowedSpecials = "!#$%&'*+-.^_`|~"
+
+	for _, r := range s {
+		switch {
+		case r >= 'A' && r <= 'Z':
+		case r >= 'a' && r <= 'z':
+		case r >= '0' && r <= '9':
+		case strings.ContainsRune(allowedSpecials, r):
+		default:
+			return false
+		}
+	}
+	return true
 }
